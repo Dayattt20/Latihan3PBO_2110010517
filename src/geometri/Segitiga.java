72af2e9ca60247ab488822276ff51fffab140ee4
Dayattt20 <dayathero20@gmail.com>
2023-07-04 13:01:23 +0800

    Membuat class lingkaran dan segitiga

---
+++ src/geometri/Segitiga.java
@@ -0,0 +1,21 @@
+package geometri;
+
+public class Segitiga {
+    private double alas;
+    private double tinggi;
+    private double sisimiring;
+
+    public Segitiga(double keliling, double luas {
+        this. keliling = Keliling;
+        this. luas = luas;
+    }
+
+    public double luas() {
+        return alas * tinggi;
+    }
+
+    public double keliling() {
+        return alas + tinggi + sisimiring;
+    }
+}  
+
